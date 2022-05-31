12a1a7a9279b75836a6ea903ba32b6a8f32eb7ce
Sarabhian <deshmukhar19.instru@coep.ac.in>
2022-05-31 19:45:19 +0530

    adding binary tree

--- cpp dsa/6 TREE/1.binary_tree.cpp
+++ cpp dsa/6 TREE/1.binary_tree.cpp
@@ -3,6 +3,9 @@
 //  BIARY TREE//
 
 #include<iostream>
+#include<queue> //for levelorder traversal
+#include<vector> //for levelorder traversal
+
 using namespace std;
 /*
 
@@ -10,10 +13,12 @@ using namespace std;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 Content of file :                                                                                                   ||
 1. create()                                                                                                         ||
-2. inorder()                                                                                                        ||
-3. postorder()                                                                                                      ||
-4. preorder()                                                                                                       ||
-5. insert()                                                                                                         ||
+2. DFS traversal :                                                                                                  ||
+        2.1. preorder()                                                                                             ||
+        2.2. inorder()                                                                                              ||
+        2.3. postorder()                                                                                            ||
+3. BFS traversal :
+.       3.1. levelorder()                                                                                           ||
 6. delete()                                                                                                         ||
 7.                                                                                                                  ||
 .                                                                                                                   ||
@@ -64,8 +69,15 @@ node* create(int lvl =-1){
        return temp;
    }
 }
+///////////////////////////////////////// DFS Traversal ////////////////////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////// preorder traversal ////////////////////////////////////////////////////////////////////////////
+/* depth first search of binary tree  consists of 3 types
+    1. preorder :            root at pre then left and right i.e.                                                 root->left->right
+    2. inorder :             traverse in a way tree looks (i.e. inorder ) ,root is in the middle i.e.             left->root->right
+    3. postorder :           root is post the left and right (root at last) i.e.                                  left->right->root         
+*/
+
+//======================================= 1.preorder traversal =================================================================================//
 
 void preorder(node * root){
     node* temp = root;
@@ -78,8 +90,7 @@ void preorder(node * root){
    
 }
 
-
-////////////////////////////////////////////////// inorder traversal /////////////////////////////////////////////////////////////////////////////
+//========================================== 2.inorder traversal ===============================================================================//
 
 void inorder(node * ROOT){
     node* temp = ROOT ;
@@ -92,7 +103,8 @@ void inorder(node * ROOT){
     inorder(temp->right);
 }
 
-/////////////////////////////////////////////////// postorder traversal /////////////////////////////////////////////////////////////////////////
+//=========================================== 3.postorder traversal =============================================================================//
+
 void postorder(node* root){
     node* temp = root;
     if( temp ==NULL){
@@ -102,6 +114,64 @@ void postorder(node* root){
     postorder(temp->right);
     cout<<temp->data<<" ";
 }
+
+/////////////////////////////////////////////// BFS (level order) traversal ////////////////////////////////////////////////////////////////////////
+/*
+// traversing a tree in horizontal way , i.e. levelwise traversal of the tree
+// we need  :
+.            queue :  store left, right and roots
+.            vector ans : to store final data
+.            level vector : to tempararily  store roots of each level
+
+ 
+Algo :  create a queue and inssert root of tree into it.
+        while( queue is not empty)
+             initiate lebvel vector
+             for(  itr  0: size of queue)
+                take out front of queue and convert it as node of tree
+                now check the node is having left or right , if exist add to queue
+                in end add node.data to level vector;
+             after one for loop complition add data of level vector to ans vector 
+        return ans  
+*/
+
+  vector<int> levelorder(node *root){
+      vector<int>ans;
+      if(root ==NULL) return ans; //return empty value
+
+      queue<node*> q;   // since queue is going to contain nodes we will use node as data type 
+      q.push(root);
+
+      while(!q.empty()){
+          vector<int> level;
+          int size = q.size(); //note we are initializing size because inside the for loop size of q is changing 
+                              //so cant use q.size() as condition in for loop
+          for(int i=0 ; i<size; i++){
+              node* temp = q.front();
+              q.pop();
+
+              if(temp->left!=NULL) q.push(temp->left);
+              if(temp->right!=NULL) q.push(temp->right);
+
+              level.push_back(temp->data);
+          }
+          ans.insert(ans.end(),level.begin(),level.end());  // add data of level vector at end of ans vector.
+
+      }
+      return ans;
+  }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int height(node* root){
+        if(root==NULL) return 0;
+        return (max(height(root->left),height(root->right))+1);
+    }
+    // diameter of a node is nothing but maximum (left_height + right_height +1 )
+    // so diameter of tree is , maximum (diamter of left node , diameter of right node ) recursively..
+ int diameter(node* root){
+        if(root == NULL) return 0;
+        int currdiameter = height(root->left) + height(root->right) +1;
+         return max(max(diameter(root->left) , diameter(root->right)), currdiameter);
+    }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -116,6 +186,34 @@ int main(){
     inorder(root);
     cout<<endl<<"postorder traversal :";
     postorder(root);
+    vector<int> l;
+    l = levelorder(root);
+    cout<<endl<<"levelorder traversal :";
+    for(int i =0 ; i< l.size(); i++){cout<<l[i]<<" ";}
     cout<<endl;
     return 0;
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//output format 
+/*
+enter -1 to exit else enter data value of new node
+1
+level :0 left node of 1:2
+level :1 left node of 2:4
+level :2 left node of 4:-1
+level :2 right node of 4:-1
+level :1 right node of 2:5
+level :2 left node of 5:-1
+level :2 right node of 5:-1
+level :0 right node of 1:3
+level :1 left node of 3:6
+level :2 left node of 6:-1
+level :2 right node of 6:-1
+level :1 right node of 3:7
+level :2 left node of 7:-1
+level :2 right node of 7:-1
+preorder traversal :1 2 4 5 3 6 7 
+inorder traversal :4 2 5 1 6 3 7 
+postorder traversal :4 5 2 6 7 3 1 
+levelorder traversal :1 2 3 4 5 6 7 
+*/
--- cpp dsa/test.cpp
+++ cpp dsa/test.cpp
@@ -1,8 +1,11 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 
 int main(){
-   
+   vector<int> ans ={} ,level ={1 ,2,3,4,5};
+   ans.insert(ans.end(),level.begin(),level.end());
+   // ans.push_back(level);
+   for(int i =0 ; i< ans.size(); i++){cout<<ans[i]<<" ";}
    return 0;
 }
