03cf449dbf4e322167a3d1aadf0f82c7833dc5f5
Sarabhian <deshmukhar19.instru@coep.ac.in>
2022-06-13 14:03:20 +0530

    using recurssion

--- cpp dsa/Algorithms/2 Recursion/3.sort_array.cpp
+++ cpp dsa/Algorithms/2 Recursion/3.sort_array.cpp
@@ -7,26 +7,27 @@ algo : 1.first write a hypothesis that our array can sort first n-1 elements in 
 .       2. find base case for array
 .       3. in induction write a function which will insert the nth element at right position in n-1 sorted array
 */
-void Insert(int key, vector<int>&arr,int n){
-    if(n==1||arr[n-1]<=key) {arr.push_back(key); return;}
+void Insert(int key, vector<int>&arr){
+    if(arr.size()==0||arr[arr.size()-1]<=key) {arr.push_back(key); return;}
 
-    int temp = arr[n-1];
-    Insert(key,arr,n-1);
+    int temp = arr[arr.size()-1];
+    arr.pop_back();
+    Insert(key,arr);
     arr.push_back(temp);
 }
-void Sort(vector<int>&arr,int n){
-    if(n==1) return;
+void Sort(vector<int>&arr){
+    if(arr.size()==1) return;
 
-    int temp = arr[n-1];
-
-    Sort(arr ,n-1);
-    Insert(temp,arr,n);
+    int temp = arr[arr.size()-1];
+    arr.pop_back();
+    Sort(arr );
+    Insert(temp,arr);
 }
 
 int main(){
-   vector<int> a = {1 ,6,3,8,2,4,8};
+   vector<int> a = {1,6,3,8,2,4,8};
    int n =7;
-   Sort(a,n);
+   Sort(a);
    for(int i =0 ;i<n;i++){cout<<a[i]<<" ";}
    cout<<endl;
    return 0;
