6ad88c59ae32feafc797781b3194490c3a2436b8
Sarabhian <deshmukhar19.instru@coep.ac.in>
2022-07-07 23:29:26 +0530

    top down subset sum

--- cpp dsa/Algorithms/6 Dynamic Programming/2.subset_sum.cpp
+++ cpp dsa/Algorithms/6 Dynamic Programming/2.subset_sum.cpp
@@ -8,20 +8,61 @@ A ={ 1,2,3,5,8,3,8,36} ,n= 8
 Sum = 12;
 
 ans : True  ... there exist a sequence {1,3,8} 
+
+A={ 1,2 ,3,4}, n=4
+sum = 12;
+ans = false;
 */
 // recursive approach 
 bool subsetSum(int arr[],int n,int target,bool ans){
-    cout<<"n: "<<n<<" target :"<<target<<" subsetsum :"<<ans<<endl;
-    if(n==0 && target ==0) {ans = 1;return ans;}
-    if(n==0) {ans= 0;return ans;}
-    if(target==0) return true; 
+    // cout<<"n: "<<n<<" target :"<<target<<" ans :"<<ans<<endl;
+
+    if(target ==0) return ans = 1;
+    if(n==0) return ans= 0;
+
     if(arr[n-1]<=target){
-        ans = subsetSum(arr,n-1,target-arr[n-1],ans) || subsetSum(arr,n-1,target,ans);
-        return ans;
+        
+        return ans = subsetSum(arr,n-1,target-arr[n-1],ans) || subsetSum(arr,n-1,target,ans);
     }
-    ans = subsetSum(arr,n-1,target,ans);
-    return ans;
+    
+    return ans = subsetSum(arr,n-1,target,ans);
 }
+
+// memoization
+ bool subsetSum_memoize( int arr[],int dp[],int n,int target){
+    
+    if(target==0) return 1;
+    if(n==0) return 0;
+   
+    if(arr[n-1]<=target){
+       return  dp[n] = subsetSum_memoize(arr,dp,n-1,target-arr[n-1]) || subsetSum_memoize(arr,dp,n-1,target); 
+    }
+    return dp[n] = subsetSum_memoize(arr,dp,n-1,target);
+ }
+
+ //  top down
+ bool subsetSum_topdown(int arr[], int n, int target){
+    vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+   
+    for(int i=0;i<n+1;i++){
+        dp[i][0]=1;
+        }
+    cout<<endl;
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<target+1;j++){
+            cout<<dp[i-1][j-1]<<" ";
+         if(arr[i-1]<= j){
+            dp[i][j] = (dp[i-1][j - arr[i-1]] || dp[i-1][j]) ;
+         } 
+         else dp[i][j]= dp[i-1][j];
+
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<"Does there exist a subset sum ? :";
+    return dp[n][target];
+ }
 int main(){
    int n,Sum;
         cout<<"enter values for n ";
@@ -36,9 +77,17 @@ int main(){
         // enter target sum 
         cout<<"enter the target sum value :";
         cin>>Sum;
-        //calling method knapSack()
-        cout<<"there exist a subset sum  :";
+       
         
-        cout<<subsetSum(A,n,Sum,0)<<endl;
+         //calling recursive approach
+        //  cout<<"Does there exist a subset sum ? :";
+        // cout<<subsetSum(A,n,Sum,0)<<endl;
+
+        // calling memoization method;
+        int dp[n+1];
+        // cout<<subsetSum_memoize(A,dp,n,Sum)<<endl;
+
+        // calling top down menthod.
+        cout<<subsetSum_topdown(A,n,Sum)<<endl;
    return 0;
 }
